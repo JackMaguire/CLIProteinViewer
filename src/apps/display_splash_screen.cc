f9121d4df5cd2d0cd7c43e7fdda1051654ceb312
Jack Maguire <jackmaguire1444@gmail.com>
2019-09-05 15:08:23 -0400

    src/apps//display_splash_screen.cc

---
+++ src/apps/display_splash_screen.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <visualize/visualize.hh>
+#include <visualize/colors.hh>
+
+using namespace CLIProteinViewer;
+using namespace CLIProteinViewer::visualize;
+using namespace CLIProteinViewer::color;
+
+int main(){
+  Screen screen;
+  std::cout << screen.height() << " x " << screen.width() << std::endl;
+  screen.set_to_splash_screen();
+  printf("\n");
+  for( int h = 0; h < screen.height(); ++h ){
+    for( int w = 0; w < screen.width(); ++w ){
+      //std::cout << screen.pixel( h, w ).r << std::endl;
+      print_nearest_color( screen.pixel( h, w ) );
+    }
+  }
+}
