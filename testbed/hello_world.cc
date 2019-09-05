a2c6d11b0c97e256da558a2839a81303e0c2f1a6
Jack Maguire <jackmaguire1444@gmail.com>
2019-09-05 09:59:38 -0400

    hello_world.cc

---
+++ testbed/hello_world.cc
@@ -0,0 +1,67 @@
+//From: https://stackoverflow.com/questions/18281412/check-keypress-in-c-on-linux
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+int main()
+{
+    struct termios oldSettings, newSettings;
+
+    tcgetattr( fileno( stdin ), &oldSettings );
+    newSettings = oldSettings;
+    newSettings.c_lflag &= (~ICANON & ~ECHO);
+    tcsetattr( fileno( stdin ), TCSANOW, &newSettings );    
+
+    while ( 1 )
+    {
+        fd_set set;
+        struct timeval tv;
+
+        tv.tv_sec = 10;
+        tv.tv_usec = 0;
+
+        FD_ZERO( &set );
+        FD_SET( fileno( stdin ), &set );
+
+        int res = select( fileno( stdin )+1, &set, NULL, NULL, &tv );
+
+        if( res > 0 )
+        {
+            char c;
+            read( fileno( stdin ), &c, 1 );
+	    std::cout << c << " " << int(c) << " " << std::endl;
+	    /*
+	    Output for LDRU:
+	    27 
+	    [ 91 
+	    D 68 
+	    27 
+	    [ 91 
+	    B 66 
+	    27 
+	    [ 91 
+	    C 67 
+	    27 
+	    [ 91 
+	    A 65
+	    */
+        }
+        else if( res < 0 )
+        {
+            perror( "select error" );
+            break;
+        }
+        else
+        {
+            printf( "Select timeout\n" );
+        }
+    }
+
+    tcsetattr( fileno( stdin ), TCSANOW, &oldSettings );
+    return 0;
+}
