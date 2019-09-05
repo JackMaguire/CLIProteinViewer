f0e80f84c6f51eb8dfa0f7f087eea6722d4cf736
Jack Maguire <jackmaguire1444@gmail.com>
2019-09-05 19:43:09 -0400

    render logic

---
+++ src/render/render.hh
@@ -0,0 +1,121 @@
+#pragma once
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <visualize/visualize.hh>
+#include <visualize/colors.hh>
+#include <representations/spheres.hh>
+#include <representations/sphere_math.hh>
+
+namespace CLIProteinViewer {
+namespace render {
+
+constexpr CAMERA_X = -10.0;
+constexpr ABS_CAMERA_X = 10.0;
+
+void
+determine_color(
+  int const chain_id,
+  double const t0,
+  visualize::Pixel & pixel
+) {
+  pixel.r = 0;
+  pixel.g = 0;
+  pixel.b = 0;
+
+  switch( chain_id ){
+  case( 0 ): pixel.r = 255; break;
+  case( 1 ): pixel.g = 255; break;
+  case( 2 ): pixel.b = 255; break;
+  case( 3 ): pixel.r = 255; pixel.g = 255; break;
+  case( 4 ): pixel.g = 255; pixel.b = 255; break;
+  default:   pixel.b = 255; pixel.r = 255; break;
+  }
+
+  //TODO multiply by t0 - ABS_CAMERA_X?
+}
+
+double
+radius_squared_for_element( char ){
+  return 4.0;
+}
+
+bool ray_intersect(
+  spheres::XYZ const & orig,
+  spheres::XYZ const & dir,
+  spheres::Sphere const & sphere,
+  double & t0
+) const {
+  spheres::XYZ const L = sphere - orig;
+  double const tca = L * dir;
+  double d2 = L*L - tca*tca;
+  double const r2 = radius_squared_for_element( sphere.atom );
+  if( d2 > r2 ) return false;
+  double const thc = sqrtf( r2 - d2);
+  t0       = tca - thc;
+  double const t1 = tca + thc;
+  if (t0 < 0) t0 = t1;
+  if (t0 < 0) return false;
+  return true;
+}
+
+void
+cast_ray(
+  spheres::XYZ const & camera_position,
+  spheres::XYZ const & ray_direction,
+  spheres::Pose const & pose,
+  //visualize::Screen & screen,
+  visualize::Pixel & pixel,
+  bool skip_hydrogens = true
+) {
+  //Sphere * closest_atom = nullptr;
+  int chain_id_for_closest_atom = -1;
+  double closest_distance = 99999;
+
+  int chain_id = 0;
+  double t0 = 0;
+  for( auto const & pair : pose.chains ){
+    for( Sphere const & s : pair.second.heavy_atoms ){
+      if( ray_intersect( camera_position, ray_direction, s, t0 ) ){
+	if( t0 < closest_distance || chain_id_for_closest_atom == -1 ){
+	  //closest_atom = &s;
+	  chain_id_for_closest_atom = chain_id;
+	  closest_distance = t0;
+	}
+      }
+    }
+    ++chain_id;
+  }
+
+  determine_color( chain_id_for_closest_atom, t0, pixel );
+}
+
+void
+draw_pose_on_screen(
+  spheres::Pose const & pose,
+  visualize::Screen & screen
+) {
+  spheres::XYZ const camera_position = { CAMERA_X, 0.0, 0.0 };
+
+  size_t const width = screen.width();
+  size_t const height = screen.height();
+
+  constexpr double fov = M_PI / 3.0;
+
+  //https://github.com/ssloy/tinyraytracer/blob/master/tinyraytracer.cpp
+  for( size_t w = 0; w < width; w++ ){
+    for( size_t h = 0; h < height; h++ ){
+      double const dir_x =  (w + 0.5) -  width/2.;
+      double const dir_y = -(h + 0.5) + height/2.;    // this flips the image at the same time
+      double const dir_z = -height/(2.*tan(fov/2.));
+      spheres::XYZ ray = { dir_x, dir_y, dir_z };
+      ray.normalize();
+
+      cast_ray( camera_position, ray, pose, screen.pixel( h, w ) );
+    }
+  }
+}
+
+}//namespace render
+}//namespace CLIProteinViewer
