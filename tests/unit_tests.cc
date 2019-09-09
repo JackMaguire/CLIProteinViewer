8b20a6577c5f0a82053b31f9fd7c9f03088a2d67
Jack Maguire <jack@med.unc.edu>
2019-09-09 15:28:33 -0400

    test3

--- tests/unit_tests.cc
+++ tests/unit_tests.cc
@@ -41,7 +41,27 @@ void run_test2(){
   assert( origin2.z == 0.0 );
 }
 
+void run_test3(){
+  Pose p;
+  p.chains[ "UNIT_TEST"  ].heavy_atoms.emplace_back( 1.0, 2.0, 3.0, 'Y' );
+  p.chains[ "UNIT_TEST2" ].heavy_atoms.emplace_back( 3.0, 2.0, 3.0, 'Z' );
+
+  //Test origin
+  XYZ const origin = p.calc_origin();
+  assert( origin.x == 2.0 );
+  assert( origin.y == 2.0 );
+  assert( origin.z == 3.0 );
+
+  //Test origin after normalization
+  p.normalize_pose();
+  XYZ const origin2 = p.calc_origin();
+  assert( origin2.x == 0.0 );
+  assert( origin2.y == 0.0 );
+  assert( origin2.z == 0.0 );
+}
+
 int main(){
   run_test1();
   run_test2();
+  run_test3();
 }
