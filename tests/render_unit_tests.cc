4cb69d4ff5032aa27e952191a52c0cc873f832de
Jack Maguire <jack@med.unc.edu>
2019-09-09 15:26:50 -0400

    test2

--- tests/render_unit_tests.cc
+++ tests/render_unit_tests.cc
@@ -15,8 +15,33 @@ void run_test1(){
   assert( origin.y == 0.0 );
   assert( origin.z == 0.0 );
 
+  //Test origin after normalization
+  p.normalize_pose();
+  XYZ const origin2 = p.calc_origin();
+  assert( origin2.x == 0.0 );
+  assert( origin2.y == 0.0 );
+  assert( origin2.z == 0.0 );
+}
+
+void run_test2(){
+  Pose p;
+  p.chains[ "UNIT_TEST" ].heavy_atoms.emplace_back( 1.0, 2.0, 3.0, 'Y' );
+
+  //Test origin
+  XYZ const origin = p.calc_origin();
+  assert( origin.x == 1.0 );
+  assert( origin.y == 2.0 );
+  assert( origin.z == 3.0 );
+
+  //Test origin after normalization
+  p.normalize_pose();
+  XYZ const origin2 = p.calc_origin();
+  assert( origin2.x == 0.0 );
+  assert( origin2.y == 0.0 );
+  assert( origin2.z == 0.0 );
 }
 
 int main(){
   run_test1();
+  run_test2();
 }
