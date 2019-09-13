65b191042e62c7ff0455983cc65ec10b42d9a0e6
Jack Maguire <jackmaguire1444@gmail.com>
2019-09-13 12:51:15 -0400

    src/apps/display_two_spheres.cc

---
+++ src/apps/display_two_spheres.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include <visualize/visualize.hh>
+#include <visualize/colors.hh>
+#include <representations/spheres.hh>
+#include <render/render.hh>
+
+#include <vector>
+
+using namespace CLIProteinViewer;
+using namespace CLIProteinViewer::visualize;
+using namespace CLIProteinViewer::color;
+using namespace CLIProteinViewer::spheres;
+
+int main( int argc, char **argv ){
+  Screen screen;
+  std::cout << screen.height() << " x " << screen.width() << std::endl;
+
+  Pose pose;
+  pose.chains[ "" ].heavy_atoms.emplace_back( 0.0, 0.0, 0.0, 'X', 1.0 );
+
+  render::draw_pose_on_screen( pose, screen );
+
+  printf("\n");
+  for( int h = 0; h < screen.height(); ++h ){
+    for( int w = 0; w < screen.width(); ++w ){
+      //std::cout << screen.pixel( h, w ).r << std::endl;
+      print_nearest_color( screen.pixel( h, w ) );
+    }
+  }
+}
