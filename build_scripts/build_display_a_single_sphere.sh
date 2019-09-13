c25b693162dfcf25f3a621b31c4892168056d57c
Jack Maguire <jackmaguire1444@gmail.com>
2019-09-13 12:31:57 -0400

    settings

--- build_scripts/build_display_a_single_sphere.sh
+++ build_scripts/build_display_a_single_sphere.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ src/apps/display_single_sphere.cc -o display_single_sphere -Isrc -O3 -std=c++17
-#g++ src/apps/display_single_sphere.cc -o display_single_sphere -Isrc -O0 -g -DDEBUG -std=c++17
+g++ src/apps/display_a_single_sphere.cc -o display_a_single_sphere -Isrc -O3 -std=c++17
+#g++ src/apps/display_a_single_sphere.cc -o display_a_single_sphere -Isrc -O0 -g -DDEBUG -std=c++17
---
+++ src/settings.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace CLIProteinViewer {
+namespace settings {
+
+constexpr double h_to_v_ratio = 0.7;
+
+}
+}
