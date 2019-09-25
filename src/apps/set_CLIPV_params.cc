ecc054d4304db09923198daecbd61347ed6f8cdf
Jack Maguire <jackmaguire1444@gmail.com>
2019-09-25 15:41:55 -0400

    src/apps/set_CLIPV_params.cc

---
+++ src/apps/set_CLIPV_params.cc
@@ -0,0 +1,213 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <iostream>
+#include <vector>
+
+#include <visualize/visualize.hh>
+#include <visualize/colors.hh>
+#include <representations/spheres.hh>
+#include <render/render.hh>
+#include <keylistener/keylistener.hh>
+#include <util.hh>
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+using namespace CLIProteinViewer;
+using namespace CLIProteinViewer::visualize;
+using namespace CLIProteinViewer::color;
+using namespace CLIProteinViewer::spheres;
+using namespace CLIProteinViewer::keylistener;
+
+int main( int argc, char **argv ){
+
+  const char *homedir;
+
+  if ((homedir = getenv("HOME")) == NULL) {
+    homedir = getpwuid(getuid())->pw_dir;
+  }
+
+  std::string new_filename( homedir );
+  new_filename += "/.clipv.settings";
+
+  std::cout << "Creating file: " << new_filename << std::endl;
+
+  CLIProteinViewer::fit_display_parameters();
+
+  Screen screen;
+  std::cout << screen.height() << " x " << screen.width() << std::endl;
+
+  Pose pose( args[1] );
+  pose.normalize_pose( true, true );
+
+
+  double x_rotation = 0.0;
+  double y_rotation = 0.0;
+  double z_rotation = 0.0;
+
+  double d_rot = M_PI / 4.0;
+  render::DisplayMode display_mode = render::DisplayMode::BB_HEAVY;
+
+  double zoomin_factor = 0.97;
+  double zoomout_factor = 1.05;
+
+  render::draw_pose_on_screen( pose, screen, display_mode );
+
+  printf("\n");
+  for( int h = 0; h < screen.height(); ++h ){
+    for( int w = 0; w < screen.width(); ++w ){
+      //std::cout << screen.pixel( h, w ).r << std::endl;
+      print_nearest_color( screen.pixel( h, w ) );
+    }
+  }
+
+  struct termios oldSettings, newSettings;
+
+  tcgetattr( fileno( stdin ), &oldSettings );
+  newSettings = oldSettings;
+  newSettings.c_lflag &= (~ICANON & ~ECHO);
+  tcsetattr( fileno( stdin ), TCSANOW, &newSettings );    
+
+  while( true ) {
+    fd_set set;
+    struct timeval tv;
+
+    tv.tv_sec = 10;
+    tv.tv_usec = 0;
+
+    FD_ZERO( &set );
+    FD_SET( fileno( stdin ), &set );
+
+    int const res = select( fileno( stdin )+1, &set, NULL, NULL, &tv );
+
+    bool repaint = false;
+
+    if( res > 0 ){
+      char c;
+      read( fileno( stdin ), &c, 1 );
+      int const command = int( c );
+      switch( parse_int( command ) ){
+
+	//Rotate:
+      case Key::UP:
+	z_rotation += d_rot;
+	repaint = true;
+	break;
+      case Key::DOWN:
+	z_rotation -= d_rot;
+	repaint = true;
+	break;
+      case Key::LEFT:
+	y_rotation += d_rot;
+	repaint = true;
+	break;
+      case Key::RIGHT:
+	y_rotation -= d_rot;
+	repaint = true;
+	break;
+      case Key::A:
+	x_rotation += d_rot;
+	repaint = true;
+	break;
+      case Key::D:
+	x_rotation -= d_rot;
+	repaint = true;
+	break;
+
+	// Zooming:
+      case Key::S:
+	settings::ZOOM *= zoomout_factor;
+	repaint = true;
+	break;
+      case Key::W:
+	settings::ZOOM *= zoomin_factor;
+	repaint = true;
+	break;
+
+	//Step Size:
+      case Key::ONE:
+	d_rot = M_PI / 16.0;
+	zoomin_factor = 0.97;
+	zoomout_factor = 1.05;
+	repaint = false;
+	break;
+      case Key::TWO:
+	d_rot = M_PI / 8.0;
+	zoomin_factor = 0.93;
+	zoomout_factor = 1.10;
+	repaint = false;
+	break;
+      case Key::THREE:
+	d_rot = M_PI / 4.0;
+	zoomin_factor = 0.8;
+	zoomout_factor = 1.5;
+	repaint = false;
+	break;
+      case Key::FOUR:
+	d_rot = M_PI / 2.0;
+	zoomin_factor = 0.5;
+	zoomout_factor = 2.0;
+	repaint = false;
+	break;
+
+	//View Mode:
+      case Key::ZERO:
+	if( display_mode != render::DisplayMode::ALLATOM ){
+	  display_mode = render::DisplayMode::ALLATOM;
+	  repaint = true;
+	}
+	break;
+      case Key::NINE:
+	if( display_mode != render::DisplayMode::HEAVY ){
+	  display_mode = render::DisplayMode::HEAVY;
+	  repaint = true;
+	}
+	break;
+      case Key::EIGHT:
+	if( display_mode != render::DisplayMode::BB_HEAVY ){
+	  display_mode = render::DisplayMode::BB_HEAVY;
+	  repaint = true;
+	}
+	break;
+      case Key::SEVEN:
+	if( display_mode != render::DisplayMode::BB_N ){
+	  display_mode = render::DisplayMode::BB_N;
+	  repaint = true;
+	}
+	break;
+
+      }
+    }
+    else if( res < 0 ){
+      perror( "select error" );
+      break;
+    }
+
+    if( repaint ){
+      Pose p( pose );
+      transform_pose( p, x_rotation, y_rotation, z_rotation );
+      render::draw_pose_on_screen( p, screen, display_mode );
+      for( int h = 0; h < screen.height(); ++h ){
+	for( int w = 0; w < screen.width(); ++w ){
+	  //std::cout << screen.pixel( h, w ).r << std::endl;
+	  print_nearest_color( screen.pixel( h, w ) );
+	}
+      }      
+    }
+
+  }
+
+  tcsetattr( fileno( stdin ), TCSANOW, &oldSettings );
+
+}
