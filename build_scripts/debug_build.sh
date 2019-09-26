a2184b3f409b3ebc763a7bbc27e12de2abb4ba43
Jack Maguire <jackmaguire1444@gmail.com>
2019-09-26 15:29:53 -0400

    better debug build

--- build_scripts/debug_build.sh
+++ build_scripts/debug_build.sh
@@ -2,7 +2,7 @@
 
 for x in "g++" "clang++"; do
     for y in "c++11" "c++14" "c++17"; do
-	$x src/apps/CLIProteinViewer.cc -o CLIProteinViewer -Isrc -O0 -g -DDEBUG -std=$y -Wall -Wextra -Wunused
-	$x src/apps/set_CLIPV_params.cc -o set_CLIPV_params -Isrc -O0 -g -DDEBUG -std=$y -Wall -Wextra -Wunused
+	$x src/apps/CLIProteinViewer.cc -o CLIProteinViewer_debug -Isrc -O0 -g -DDEBUG -std=$y -Wall -Wextra -Wunused
+	$x src/apps/set_CLIPV_params.cc -o set_CLIPV_params_debug -Isrc -O0 -g -DDEBUG -std=$y -Wall -Wextra -Wunused
     done
 done
