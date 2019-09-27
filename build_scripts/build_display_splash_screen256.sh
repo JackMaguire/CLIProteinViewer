d9cfa95fd4f5f4405e736b308ab729dee28726f5
Jack Maguire <jack@med.unc.edu>
2019-09-26 20:40:40 -0400

    naming

--- build_scripts/build_display_splash_screen256.sh
+++ build_scripts/build_display_splash_screen256.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ src/apps/display_splash_screen256.cc -o bin/display_splash_screen256 -Isrc -O3
+g++ src/apps/display_splash_screen_256.cc -o bin/display_splash_screen256 -Isrc -O3
